0f44d67ccb5fcc0ca68398257e07443d98dde852
Paul Sarando <psarando@cyverse.org>
2016-12-12 16:41:53 -0700

    Initial clipboardFunctions app.

--- react-components/src/index.js
+++ react-components/src/index.js
@@ -4,6 +4,9 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <App text={
+`Can we copy?
+Yes we can!!!`
+    }/>,
+    document.getElementById('root')
 );
